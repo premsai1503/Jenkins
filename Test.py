45d3907dacc8cf93c34109d677c1deca4c53afa0
prem sai <56483749+premsai1503@users.noreply.github.com>
2021-11-25 22:15:53 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,16 +2,22 @@
 # Test case for adding two numbers
 import unittest
 
-from Prog1 import summation
+from IMT2019067_prog import power
 
-class TestSum(unittest.TestCase):
-    def test_list_int(self):
-        """
-        Test case to add two numbers
-        """
-        data = [23, 32]
-        result = summation(data)
-        self.assertEqual(result, 55)
+class Test_power(unittest.TestCase):
+    def test_1(self):
+        data = [2,4]
+        result = power(data)
+        self.assertEqual(result, 16)
+    def test_2(self):
+        data = [64,0.5]
+        result = power(data)
+        self.assertEqual(result, 8)
+        
+    def test_1(self):
+        data = [9,2]
+        result = power(data)
+        self.assertEqual(result, 80)
 
 if __name__ == '__main__':
     unittest.main()
