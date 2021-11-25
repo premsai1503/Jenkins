18f94321ac3b9b68a6ac8ba52e6013c3b2ccbca0
prem sai <56483749+premsai1503@users.noreply.github.com>
2021-11-25 22:20:18 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -7,16 +7,16 @@ from IMT2019067_prog import power
 class Test_power(unittest.TestCase):
     def test_1(self):
         data = [2,4]
-        result = power(data)
+        result = power(2,4)
         self.assertEqual(result, 16)
     def test_2(self):
         data = [64,0.5]
-        result = power(data)
+        result = power(64,0.5)
         self.assertEqual(result, 8)
         
     def test_1(self):
         data = [9,2]
-        result = power(data)
+        result = power(9,2)
         self.assertEqual(result, 80)
 
 if __name__ == '__main__':
